6fe19d6334963e089d43d7ef86e680d1016e2b54
Erick Falconi <e.alexander.f013@gmail.com>
2021-11-10 20:39:48 -0500

    finished assigned part for intro

--- DivByZero_dream_pulse/js/index.js
+++ DivByZero_dream_pulse/js/index.js
@@ -1,1 +1,5 @@
 alert("Working")
+
+//<p id="intropara1" class="tm-color-gray">  <!--todo: Add customer company introduction text-->
+let intro = document.getElementById('intropara1')
+intro.textContent = "Alliance was founded in 1931 by Albert Daniels.";
